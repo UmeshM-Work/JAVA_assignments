364d2a6acb120e485002cb969d85cb5de4f3c7b9
UMESH WORK <65343874+UmeshM-Work@users.noreply.github.com>
2020-10-06 22:47:14 +0530

    Update Assignment9.java

--- Assignment9.java
+++ Assignment9.java
@@ -222,3 +222,149 @@ public class Q62 {
 	}
 
 }
+
+/*Q63.Write a program to count how many times character ‘t’ occurs in a file.*/
+
+import java.io.*;
+
+public class Q63 {
+
+	public static void main(String[] args) throws IOException {
+		FileOutputStream f1=new FileOutputStream("E:\\corejava\\DayFile2\\Demo3.txt");
+		 String s=" Hi This is demo of FileOutputStream:\n ";
+		 //f1.write(s); error as byte by byte
+		 for(int i=0;i<s.length();i++)
+		 {
+			 f1.write(s.charAt(i));
+			 
+		 }
+         f1.close();
+         System.out.println("Success");
+         int ch=-1;
+         int count=0;
+         FileInputStream f2=new FileInputStream("E:\\corejava\\DayFile2\\Demo3.txt");
+         {
+        	 while((ch=f2.read()) != -1)
+            {
+	           System.out.print((char)ch);
+	           char ch1=(char)ch;
+	           if(ch1=='t')
+	           {
+	        	   count++;
+	           }
+            }
+        	
+         }
+         System.out.println(count);
+         f2.close();
+	}
+
+}
+-------------------------------------------------------------------------------------------------------------------------------
+/*Q64.Write a program to count no of words in a text file and average word size.*/
+
+import java.io.*;
+
+public class Q64 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+        FileInputStream f1=new FileInputStream("E:\\corejava\\DayFile2\\Demo3.txt");
+              
+	}
+}
+-------------------------------------------------------------------------------------------------------------------------------
+/*Q65.Write a program to count number of bytes in a image file(jpeg/png/gif).  Also find how much time it will take to upload the file on server if internet speed is 256 bps(bits per second).*/
+
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
+public class Q65 {
+
+	public static void main(String[] args) throws IOException{
+		String dirName="C:\\Users\\Acer\\OneDrive\\Pictures\\Screenshots";
+		ByteArrayOutputStream baos=new ByteArrayOutputStream(1000);
+		BufferedImage img=ImageIO.read(new File(dirName,"dip.png"));
+		ImageIO.write(img, "jpg", baos);
+		baos.flush();
+ 
+		String base64String=Base64.encode(baos.toByteArray());
+		baos.close();
+ 
+		byte[] bytearray = Base64.decode(base64String);
+ 
+		BufferedImage imag=ImageIO.read(new ByteArrayInputStream(bytearray));
+		ImageIO.write(imag, "jpg", new File(dirName,"snap.jpg"));
+	}
+
+}
+-------------------------------------------------------------------------------------------------------------------------------
+/*Q66.Write a program to store your shopping details in a binary file(shopping.dat) with information itemName, price, quantity. (Use ObjectOutputStream to store Item class object ).*/
+
+
+
+//package p1;
+import java.io.*;
+
+class Shopping implements Serializable{
+		private String itemName;
+		private float price;
+	        public int quantity;
+		public Shopping () {
+
+		}
+		public Shopping(String itemName,float price,int quantity ) {
+			this.itemName = itemName;
+			this.price = price;
+	                this.quantity=quantity;
+		}
+		@Override
+		public String toString() {
+			return itemName + " " + price + " " + quantity;
+		}
+		
+	}
+	
+	public class Q66 {
+
+		public static void main(String[] args) throws IOException,ClassNotFoundException {
+			FileOutputStream f = 
+					new FileOutputStream("D:\\core_java\\codes\\shopping.dat");
+			
+			ObjectOutputStream o = new ObjectOutputStream(f);
+			
+			o.writeObject(new Shopping("abc",2300f,5));
+			o.close();	
+			f.close();
+			System.out.println("success");
+		}
+
+	}
+-------------------------------------------------------------------------------------------------------------------------------
+/*Q67.Write a program to read data from shopping.dat file creted in above problem and find total money spent on all shopping items. . (Use ObjectInputStream to read Item class object).*/
+
+import java.io.*;
+
+public class Q67 {
+
+	public static void main(String[] args)throws IOException, ClassNotFoundException {
+		FileInputStream f = 
+				new FileInputStream("D:\\core_java\\codes\\shopping.dat");
+		
+		ObjectInputStream o = new ObjectInputStream(f);
+		
+		Shopping s = (Shopping)o.readObject();
+		
+		System.out.println(s); 
+				
+		f.close();
+		System.out.println("success");
+	}
+
+}
